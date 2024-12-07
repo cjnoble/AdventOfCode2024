fc5701d7ca109432a4ca172d44ec8b0e5765d1fe
Chris Noble <NobleChrisJ@gmail.com>
2024-12-07 16:23:20 +0000

    Update Day Template

--- day_template.py
+++ day_template.py
@@ -1,4 +1,4 @@
-import re
+import time
 
 def read_text_file (file_path):
 
@@ -21,5 +21,10 @@ if __name__ == "__main__":
 
     DAY = "02"
     data = read_text_file(f"{DAY}.txt")
+    part_1_start = time.time()
     print(part_1(data))
+    print(f"Part 1 finished in {time.time() - part_1_start} s")
+
+    part_2_start = time.time()
     print(part_2(data))
+    print(f"Part 2 finished in {time.time() - part_2_start} s")
