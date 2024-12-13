e74dc1a640ef61d9fa61d61a58baff712a012b2a
Christopher Noble <Christopher.Noble@arup.com>
2024-12-13 17:39:18 +0000

    Started day 13

---
+++ day_13.py
@@ -0,0 +1,33 @@
+import time
+import numpy as np
+
+def read_text_file (file_path):
+
+    with open(file_path, "r") as f:
+        data = f.readlines()
+        data = [line.replace("\n","") for line in data]
+
+    return data
+
+
+def part_1(data):
+
+    np.linalg
+
+    return
+
+def part_2(data):
+
+    return
+
+if __name__ == "__main__":
+
+    DAY = "13"
+    data = read_text_file(f"{DAY}.txt")
+    part_1_start = time.time()
+    print(part_1(data))
+    print(f"Part 1 finished in {time.time() - part_1_start} s")
+
+    part_2_start = time.time()
+    print(part_2(data))
+    print(f"Part 2 finished in {time.time() - part_2_start} s")
